eff00434bb567ee0119b7188e9219cfa0923a088
frank <264430662@qq.com>
2026-02-21 15:06:29 +0800

    feat: update icon and description

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -19,7 +19,7 @@ const Footer: React.FC = () => {
         {
           key: 'github',
           title: <GithubOutlined />,
-          href: 'https://github.com/ant-design/ant-design-pro',
+          href: 'https://github.com/frankSoft365/frank-api-frontend#',
           blankTarget: true,
         },
         {
